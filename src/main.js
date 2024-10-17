4de46b7672601994c3bc4eb709f14c6286fb9f5c
SlonyRufus <33912376+slonyrufus@users.noreply.github.com>
2024-10-17 09:38:14 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-<<<<<<< Updated upstream
-import '@/services/components.js'
-=======
 // import Components from '@/services/components.js'
->>>>>>> Stashed changes
 import '@/css/styles.css';
 
 Vue.config.productionTip = false
