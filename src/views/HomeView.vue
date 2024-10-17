656180b9c75663b995535ce4e8dd212e8d9920a2
Plocala <thtmjunior@gmail.com>
2024-10-17 06:52:28 -0300

    nnn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,17 +14,42 @@
             <aside class="sidebar-home">
                 <IconsComponent />
             </aside>
+            <div>
+                <h1>Pacientes</h1>
+                <ul>
+                    <li v-for="paciente in pacientes" :key="paciente.id">{{ paciente.nome }}</li>
+                </ul>
+            </div>
         </main>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
     import IconsComponent from '@/components/IconsComponent.vue';
 
     export default {
         name: 'HomeView',
         components: {
             IconsComponent,
-        }
-    }
+        },
+        Data() {
+            return {
+                pacientes: [],
+            }
+        },
+        mounted() {
+            this.buscarPacientes();
+        },
+        methods: {
+            async buscarPacientes() {
+                try {
+                    const resposta = await axios.get('http://localhost:8080/api/pacientes');
+                    this.pacientes = resposta.data;
+                } catch (erro) {
+                    console.error('Error ao buscar pacientes', erro);
+                }
+            },
+        },
+    };
 </script>
